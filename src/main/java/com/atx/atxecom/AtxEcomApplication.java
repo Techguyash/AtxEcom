a80c06bbc30c086e517e846a8f3afaf3f8e35e1f
techguyash <techguyash@gmail.com>
2025-03-09 15:23:09 +0530

    initial commit

---
+++ src/main/java/com/atx/atxecom/AtxEcomApplication.java
@@ -0,0 +1,13 @@
+package com.atx.atxecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtxEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtxEcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atx/atxecom/AtxEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atx.atxecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtxEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
